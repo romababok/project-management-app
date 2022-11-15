e0e60e640056d2d60866a1f2da06bae21c956d3a
romanbabok <roman.babok18@gmail.com>
2022-11-15 22:22:34 +0300

    feat:  add boards-slice and add boardsAPI

---
+++ src/api/boards.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { baseApiUrl } from ".";
+
+export interface CreateBoardRequest {
+  title: string;
+  owner: string;
+  users: string[];
+}
+
+export const getAllBoardsAPI = async () =>
+  await axios.get(baseApiUrl + "/boards");
+
+export const createBoardAPI = async (request: CreateBoardRequest) => {
+  return await axios.post(baseApiUrl + "/boards", request);
+};
+export const getBoardByIdAPI = async (boardId: string) => {
+  return await axios.get(baseApiUrl + `/boards/${boardId}`);
+};
---
+++ src/features/boards/boards-slice.ts
@@ -0,0 +1,117 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  createBoardAPI,
+  CreateBoardRequest,
+  getBoardByIdAPI,
+  getAllBoardsAPI,
+} from "../../api";
+import axios from "axios";
+import { notification } from "antd";
+
+export interface Board {
+  _id: string;
+  title: string;
+  owner: string;
+  users: string[];
+}
+
+export interface BoardsState {
+  boards: Board[];
+  currentBoard: Board | null;
+  status: "idle" | "loading" | "failed";
+}
+
+const initialState: BoardsState = {
+  boards: [{ title: "", users: [], owner: "", _id: "" }],
+  currentBoard: null,
+  status: "idle",
+};
+
+export const getAllBoards = createAsyncThunk("boards/getBoards", async () => {
+  try {
+    const response = await getAllBoardsAPI();
+    return response.data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      notification.error({
+        message: "Request failed with code " + err.response?.status,
+        description: err.response?.data.message,
+      });
+    }
+  }
+});
+
+export const createBoard = createAsyncThunk(
+  "boards/createBoard",
+  async (request: CreateBoardRequest, thunkApi) => {
+    try {
+      const response = await createBoardAPI(request);
+      return response.data;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        notification.error({
+          message: "Request failed with code " + err.response?.status,
+          description: err.response?.data.message,
+        });
+      }
+    }
+  }
+);
+
+export const getBoardById = createAsyncThunk(
+  "boards/getBoardById",
+  async (boardId: string) => {
+    try {
+      const response = await getBoardByIdAPI(boardId);
+      return response.data;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        notification.error({
+          message: "Request failed with code " + err.response?.status,
+          description: err.response?.data.message,
+        });
+      }
+    }
+  }
+);
+
+export const boardsSlice = createSlice({
+  name: "boards",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllBoards.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getAllBoards.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.boards = [...action.payload, initialState.boards];
+      })
+      .addCase(getAllBoards.rejected, (state) => {
+        state.status = "failed";
+      })
+      .addCase(createBoard.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(createBoard.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.boards.push(action.payload);
+      })
+      .addCase(createBoard.rejected, (state) => {
+        state.status = "failed";
+      })
+      .addCase(getBoardById.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getBoardById.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.currentBoard = action.payload;
+      })
+      .addCase(getBoardById.rejected, (state) => {
+        state.status = "failed";
+      });
+  },
+});
+
+export default boardsSlice.reducer;
