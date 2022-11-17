1fb0c1406c71bdbf44705c57de6088c7f27e8162
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-11-17 23:10:31 +0100

    feat: change page-layout component

--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -1,55 +1,16 @@
-import { Layout, Menu } from 'antd';
-import React from 'react';
-import { Link, Outlet, useLocation } from 'react-router-dom';
-import { Footer, Header } from 'antd/lib/layout/layout';
-import { useAppSelector } from '../app/hooks';
-import { UserOutlined } from '@ant-design/icons';
+import { Layout } from "antd";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import FooterOfApp from "./Footer/Footer";
+import HeaderOfApp from "./Header/Header";
 
 export const PageLayout: React.FC = () => {
-  const location = useLocation();
-  const token = useAppSelector((state) => state.auth.userData?.token);
 
   return (
-    <Layout className="layout">
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" selectedKeys={[location.pathname]}>
-          <Menu.Item key="/">
-            <Link to="/">Welcome</Link>
-          </Menu.Item>
-          {token && (
-            <Menu.Item key="/boards">
-              <Link to="/boards">Boards</Link>
-            </Menu.Item>
-          )}
-
-          {!token ? (
-            <>
-              <Menu.Item style={{ marginLeft: 'auto' }} key="/login">
-                <Link to="/login">Login</Link>
-              </Menu.Item>
-              <Menu.Item key="/registration">
-                <Link to="/registration">Sing Up</Link>
-              </Menu.Item>
-            </>
-          ) : (
-            <Menu.SubMenu
-              key="/user"
-              title={<UserOutlined style={{ fontSize: '16px' }} />}
-              style={{ marginLeft: 'auto' }}
-            >
-              <Menu.Item style={{ marginLeft: 'auto' }} key="/profile">
-                <Link to="/profile">Profile</Link>
-              </Menu.Item>
-              <Menu.Item key="/logout">
-                <Link to="/welcome">Logout</Link>
-              </Menu.Item>
-            </Menu.SubMenu>
-          )}
-        </Menu>
-      </Header>
+    <Layout style={{ minHeight: "100vh" }} className="layout">
+      <HeaderOfApp/>
       <Outlet />
-      <Footer style={{ textAlign: 'center' }}></Footer>
+      <FooterOfApp/>
     </Layout>
   );
 };
