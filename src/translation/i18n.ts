5893dbe86e4b40dc999837b6cfe59178190050fb
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-11-22 13:57:54 +0100

    refactor: change lng option in i18n initialization

--- src/translation/i18n.ts
+++ src/translation/i18n.ts
@@ -14,7 +14,7 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: localStorage.getItem('lang')!,
+  lng: localStorage.getItem('lang')! || 'en',
   fallbackLng: 'en',
   keySeparator: false,
   interpolation: {
