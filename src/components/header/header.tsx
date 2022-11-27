7c03f79c2d3a5c383ae1ba6f4f442e12334bd5d7
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-11-27 21:40:19 +0100

    fix: add sticky header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Header } from 'antd/lib/layout/layout';
-import { Menu, Button, Drawer, Switch, Typography, Space } from 'antd';
+import { Menu, Button, Drawer, Switch, Typography, Space, MenuProps, MenuTheme } from 'antd';
 import i18n from '../../translation/i18n';
 import { useTranslation } from 'react-i18next';
 import { Link, useLocation } from 'react-router-dom';
@@ -11,6 +11,7 @@ import {
   LogoutOutlined,
   MenuOutlined,
   PlusCircleOutlined,
+  PlusOutlined,
   PushpinOutlined,
   ToolOutlined,
   UserOutlined,
@@ -19,6 +20,25 @@ import styles from './header.module.scss';
 import { getUserData, logOut } from '../../features/auth/auth-slice';
 
 const { Text } = Typography;
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key?: React.Key | null,
+  className?: string,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group'
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+    className,
+  } as MenuItem;
+}
 
 export const HeaderOfApp: React.FC = () => {
   const location = useLocation();
@@ -30,6 +50,15 @@ export const HeaderOfApp: React.FC = () => {
 
   const token = localStorage.getItem('token');
   const dispatch = useAppDispatch();
+  const [theme, setTheme] = useState<MenuTheme>('dark');
+
+  const followScroll = () => {
+    if (window.pageYOffset > 70) {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  };
 
   useEffect(() => {
     if (token) {
@@ -41,6 +70,20 @@ export const HeaderOfApp: React.FC = () => {
     localStorage.setItem('lang', lang);
     i18n.changeLanguage(lang);
   }, [lang]);
+
+  useEffect(() => {
+    if (userId) {
+      window.addEventListener('scroll', () => {
+        followScroll();
+      });
+    }
+
+    return () => {
+      window.removeEventListener('scroll', () => {
+        followScroll();
+      });
+    };
+  }, [userId]);
 
   const showDrawer = () => {
     setOpen(true);
@@ -58,8 +101,41 @@ export const HeaderOfApp: React.FC = () => {
   };
   const { t } = useTranslation();
 
+  const itemsWithoutId = [
+    getItem(<Link to="/">{t('Header welcome link')}</Link>, '/'),
+    getItem(<Link to="/login">{t('Header login link')}</Link>, '/login', styles.menu__login),
+    getItem(<Link to="/registration">{t('Header sign up link')}</Link>, '/registration'),
+  ];
+
+  const itemsWithId = [
+    getItem(<Link to="/">{t('Header welcome link')}</Link>, '/'),
+    getItem(<Link to="">{t('Header create new board')}</Link>, '', styles.menu__main),
+    getItem(<Link to="/boards">{t('Header go to main page')}</Link>, '/boards'),
+    getItem('', 'user', styles.menu__user, <UserOutlined className={styles.menu__userIcon} />, [
+      getItem(
+        <Link to="/profile" className={styles.user__profile}>
+          {t('Header edit profile link')}
+        </Link>,
+        '/profile',
+        styles.user__profile
+      ),
+      getItem(
+        <Link onClick={() => dispatch(logOut())} className={styles.user__profile} to="/">
+          {t('Header logout link')}
+        </Link>,
+        '/logout',
+        styles.user__profile
+      ),
+    ]),
+  ];
+
   return (
-    <Header className={styles.header}>
+    <Header
+      className={
+        userId ? (theme === 'light' ? styles.sticky__white : styles.sticky__header) : styles.header
+      }
+      style={{ backgroundColor: theme === 'light' ? 'white' : '#001529' }}
+    >
       <Link className={styles.logo__link} to="/">
         <div className={styles.main__logo} />
       </Link>
@@ -67,7 +143,7 @@ export const HeaderOfApp: React.FC = () => {
         className={styles.menu__btn}
         type="primary"
         shape="circle"
-        icon={<MenuOutlined className={styles.menu__icon} style={{ fontSize: '18px' }} />}
+        icon={<MenuOutlined className={styles.menu__icon} />}
         onClick={showDrawer}
       ></Button>
       <Drawer width={'320px'} placement="right" onClose={onClose} open={open}>
@@ -88,13 +164,23 @@ export const HeaderOfApp: React.FC = () => {
             </>
           ) : (
             <>
+              <Link to="" onClick={onClose} className={styles.drawer__link}>
+                <PlusOutlined className={styles.link__icon} /> {t('Header create new board')}
+              </Link>
               <Link to="/boards" onClick={onClose} className={styles.drawer__link}>
-                <PushpinOutlined className={styles.link__icon} /> {t('Header board link')}
+                <PushpinOutlined className={styles.link__icon} /> {t('Header go to main page')}
               </Link>
               <Link to="/profile" onClick={onClose} className={styles.drawer__link}>
                 <ToolOutlined className={styles.link__icon} /> {t('Header edit profile link')}
               </Link>
-              <Link to="/welcome" onClick={onClose} className={styles.drawer__link}>
+              <Link
+                to="/"
+                onClick={() => {
+                  dispatch(logOut());
+                  onClose();
+                }}
+                className={styles.drawer__link}
+              >
                 <LogoutOutlined className={styles.link__icon} />
                 {t('Header logout link')}
               </Link>
@@ -104,56 +190,21 @@ export const HeaderOfApp: React.FC = () => {
       </Drawer>
 
       <Menu
-        theme="dark"
-        className={styles.header__menu}
+        theme={theme}
+        className={theme === 'light' ? styles.header__menu_light : styles.header__menu}
         mode="horizontal"
         selectedKeys={[location.pathname]}
-      >
-        <Menu.Item key="/">
-          <Link to="/">{t('Header welcome link')}</Link>
-        </Menu.Item>
-        {userId && (
-          <Menu.Item key="/boards">
-            <Link to="/boards">{t('Header board link')}</Link>
-          </Menu.Item>
-        )}
-        {!userId ? (
-          <>
-            <Menu.Item className={styles.menu__login} key="/login">
-              <Link to="/login">{t('Header login link')}</Link>
-            </Menu.Item>
-            <Menu.Item key="/registration">
-              <Link to="/registration">{t('Header sign up link')}</Link>
-            </Menu.Item>
-          </>
-        ) : (
-          <Menu.SubMenu
-            key="/user"
-            title={<UserOutlined className={styles.menu__userIcon} />}
-            className={styles.menu__user}
-          >
-            <Menu.Item className={styles.user__profile} key="/profile">
-              <Link to="/profile">{t('Header edit profile link')}</Link>
-            </Menu.Item>
-            <Menu.Item
-              onClick={() => dispatch(logOut())}
-              key="/logout"
-              className={styles.user__profile}
-            >
-              {t('Header logout link')}
-            </Menu.Item>
-          </Menu.SubMenu>
-        )}
-      </Menu>
+        items={userId ? itemsWithId : itemsWithoutId}
+      />
       <div className={userId ? styles.language__blockToken : styles.language__block}>
         <Space>
-          <Text className={styles.language}>En</Text>
+          <Text className={theme === 'dark' ? styles.language_light : styles.language}>En</Text>
           <Switch
             className={styles.language__switcher}
             defaultChecked={lang !== 'en'}
             onChange={onChange}
           />
-          <Text className={styles.language}>Ru</Text>
+          <Text className={theme === 'dark' ? styles.language_light : styles.language}>Ru</Text>
         </Space>
       </div>
     </Header>
