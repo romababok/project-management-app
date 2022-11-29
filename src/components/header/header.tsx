45a22245dedb1e9ef87ffbf3d23a95b05a02e6a3
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-11-29 20:16:54 +0100

    feat: remove getItem function and create languages array

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Header } from 'antd/lib/layout/layout';
-import { Menu, Button, Drawer, MenuProps, MenuTheme, Select, Avatar } from 'antd';
+import { Menu, Button, Drawer, MenuTheme, Select, Avatar } from 'antd';
 import i18n from '../../translation/i18n';
 import { useTranslation } from 'react-i18next';
 import { Link, useLocation } from 'react-router-dom';
@@ -18,25 +18,24 @@ import {
 import styles from './header.module.scss';
 import { getUserData, logOut } from '../../features/auth/auth-slice';
 
-type MenuItem = Required<MenuProps>['items'][number];
-
-const getItem = (
-  label: React.ReactNode,
-  key?: React.Key | null,
-  className?: string,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: 'group'
-): MenuItem => {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-    className,
-  } as MenuItem;
-};
+const languages = [
+  {
+    value: 'en',
+    label: 'EN',
+    className: styles.select__option,
+    style: { fontWeight: '700' },
+  },
+  {
+    value: 'ru',
+    label: 'RU',
+    style: { fontWeight: '700' },
+  },
+  {
+    value: 'by',
+    label: 'BY',
+    style: { fontWeight: '700' },
+  },
+];
 
 export const HeaderOfApp: React.FC = () => {
   const location = useLocation();
@@ -67,12 +66,10 @@ export const HeaderOfApp: React.FC = () => {
     if (userId) {
       if (isLightThemeSelected) {
         return styles.sticky__white;
-      } else {
-        return styles.sticky__header;
       }
-    } else {
-      return styles.header;
+      return styles.sticky__header;
     }
+    return styles.header;
   };
 
   useEffect(() => {
@@ -99,9 +96,6 @@ export const HeaderOfApp: React.FC = () => {
       });
     };
   }, [userId]);
-  useEffect(() => {
-    document.addEventListener('touchstart', () => {}, { passive: true });
-  }, []);
 
   const showDrawer = () => {
     setOpen(true);
@@ -116,37 +110,48 @@ export const HeaderOfApp: React.FC = () => {
   const { t } = useTranslation();
 
   const itemsWithoutId = [
-    getItem(<Link to="/">{t('Header welcome link')}</Link>, '/'),
-    getItem(<Link to="/login">{t('Header login link')}</Link>, '/login', styles.menu__login),
-    getItem(<Link to="/registration">{t('Header sign up link')}</Link>, '/registration'),
+    { label: <Link to="/">{t('Header welcome link')}</Link>, key: '/' },
+    {
+      label: <Link to="/login">{t('Header login link')}</Link>,
+      key: '/login',
+      className: styles.menu__login,
+    },
+    { label: <Link to="/registration">{t('Header sign up link')}</Link>, key: '/registration' },
   ];
 
   const itemsWithId = [
-    getItem(<Link to="/">{t('Header welcome link')}</Link>, '/'),
-    getItem(<Link to="">{t('Header create new board')}</Link>, '', styles.menu__main),
-    getItem(<Link to="/boards">{t('Header go to main page')}</Link>, '/boards'),
-    getItem(
-      <Avatar className={styles.menu__userIcon}>{name.slice(0, 1).toUpperCase()}</Avatar>,
-      'user',
-      styles.menu__user,
-      '',
-      [
-        getItem(
-          <Link to="/profile" className={styles.user__profile}>
-            {t('Header edit profile link')}
-          </Link>,
-          '/profile',
-          styles.user__profile
-        ),
-        getItem(
-          <Link onClick={() => dispatch(logOut())} className={styles.user__profile} to="/">
-            {t('Header logout link')}
-          </Link>,
-          '/logout',
-          styles.user__profile
-        ),
-      ]
-    ),
+    { label: <Link to="/">{t('Header welcome link')}</Link>, key: '/' },
+    {
+      label: <Link to="">{t('Header create new board')}</Link>,
+      key: '',
+      className: styles.menu__main,
+    },
+    { label: <Link to="/boards">{t('Header go to main page')}</Link>, key: '/boards' },
+    {
+      label: <Avatar className={styles.menu__userIcon}>{name.slice(0, 1).toUpperCase()}</Avatar>,
+      key: 'user',
+      className: styles.menu__user,
+      children: [
+        {
+          label: (
+            <Link to="/profile" className={styles.user__profile}>
+              {t('Header edit profile link')}
+            </Link>
+          ),
+          key: '/profile',
+          className: styles.user__profile,
+        },
+        {
+          label: (
+            <Link onClick={() => dispatch(logOut())} className={styles.user__profile} to="/">
+              {t('Header logout link')}
+            </Link>
+          ),
+          key: '/logout',
+          className: styles.user__profile,
+        },
+      ],
+    },
   ];
 
   return (
@@ -209,27 +214,9 @@ export const HeaderOfApp: React.FC = () => {
       />
       <Select
         defaultValue={lang}
-        style={{ width: 70 }}
         className={userId ? styles.language__blockToken : styles.language__block}
         onChange={handleChange}
-        options={[
-          {
-            value: 'en',
-            label: 'EN',
-            className: styles.select__option,
-            style: { fontWeight: '700' },
-          },
-          {
-            value: 'ru',
-            label: 'RU',
-            style: { fontWeight: '700' },
-          },
-          {
-            value: 'by',
-            label: 'BY',
-            style: { fontWeight: '700' },
-          },
-        ]}
+        options={languages}
       />
     </Header>
   );
